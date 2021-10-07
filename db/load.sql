30642456e3e55a42c73df51a410a4938739a13d1
Jack Lichtenstein <jsl61@duke.edu>
2021-10-07 17:18:22 -0400

    modified create.sql; started inititial creation of sample data

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,6 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Buyers FROM 'data/Buyers.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Sellers FROM 'data/Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Category FROM 'data/Category.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
