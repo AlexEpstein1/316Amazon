b6f11dd4050891116beb49df049693eee2f6e680
Jack Lichtenstein <jsl61@duke.edu>
2021-11-01 20:47:02 -0400

    add dropdown menu; add initial product review html; still no backend work done

--- app/index.py
+++ app/index.py
@@ -1,9 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import current_user
 import datetime
 
 from .models.product import Product
 from .models.purchase import Purchase
+from .models.seller_review import SellerReview
+from .models.product_review import ProductReview
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -12,7 +14,7 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     # get all available products for sale:
-    products = Product.get_all(True)
+    products = Product.get_all(available=True)
     # find the products current user has bought:
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_buyer_id_since(
@@ -24,7 +26,22 @@ def index():
                            avail_products=products,
                            purchase_history=purchases)
 
-@bp.route('/submit_prod_review',methods = ['POST', 'GET'])
-def submit_prod_review():
-    # pass in product_id, buyer_id
-    return render_template('submit_prod_review.html')
+@bp.route('/write_prod_review/<product_id>', methods = ['POST', 'GET'])
+def write_prod_review(product_id):
+    # print(product_id)
+
+    products = Product.get_all(available=False, id=product_id)
+    # print(products)
+
+    return render_template('submit_prod_review.html',
+                           product_id = product_id,
+                           products = products[0])
+
+@bp.route('/add_prod_review/<product_id>/', methods = ['POST', 'GET'])
+def add_prod_review(product_id):
+    print(product_id)
+
+    product_review = ProductReview.get(user_id = current_user.id)
+    print(product_review)
+
+    return None
--- app/models/product.py
+++ app/models/product.py
@@ -11,23 +11,34 @@ class Product:
         self.image_file = image_file
         self.available = available
 
+    # def __repr__():
+    #     return ""
+
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT id, name, price, cat_name, description, image_file, available
-FROM Products
-WHERE id = :id
-''',
-                              id=id)
+        SELECT id, name, price, cat_name, description, image_file, available
+        FROM Products
+        WHERE id = :id
+        ''',
+        id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
-    def get_all(available=True):
-        rows = app.db.execute('''
-SELECT id, name, price, cat_name, description, image_file, available
-FROM Products
-WHERE available = :available
-''',
-                              available=available)
-        print(rows[0])
-        return [Product(*row) for row in rows]
+    def get_all(available=True, id=None):
+        if id is None:
+            rows = app.db.execute('''
+            SELECT id, name, price, cat_name, description, image_file, available
+            FROM Products
+            WHERE available = {0}
+            '''.format(available),
+            available=available)
+            return [Product(*row) for row in rows]
+        else:
+            rows = app.db.execute('''
+            SELECT DISTINCT id, name, price, cat_name, description, image_file, available
+            FROM Products
+            WHERE id = :id
+            ''',
+            id=id)
+            return [Product(*row) for row in rows]
